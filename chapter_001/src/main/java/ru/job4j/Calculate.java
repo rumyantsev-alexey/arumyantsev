fd965f11b53751886a74b3d633128357d5e9a15a
Alexey Rumyantsev <telesyn73@mail.ru>
2017-11-07 14:25:24 +0200

    1.3. JavaDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,13 @@
 package ru.job4j;
 
-public class Calculate 
-{
-    public static void main(String[] args) 
-	{
+/**
+ *Class Calculate для первой части
+  *@author Rumyantsev
+  *@since 06.11.2017
+*/
+
+public class Calculate {
+    public static void main(String[] args) 	{
         System.out.println("Hello World");
     }
 }

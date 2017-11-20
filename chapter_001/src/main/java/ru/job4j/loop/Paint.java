66c9c8797236e1f871bd0fbb545fa7c0df59a1d7
Alexey Rumyantsev <telesyn73@mail.ru>
2017-11-20 23:10:30 +0200

    Исправления по 4.4

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -9,27 +9,26 @@ public class Paint {
      * @return строку из Х в середине и пробелов
      */
     private String PrintLine(int aster,int maxaster){
-        String str="";
+        StringBuilder builder = new StringBuilder();
         int spaces=(maxaster-aster)/2;
         for (int i=1; (spaces>=0)&(i<=aster);i++){
-           str=str+"X";
+           builder.append("X");
         }
         for(int j=1;(spaces>0)&(j<=spaces);j++){
-         str=" " +str+" ";
+            builder.insert(0," ");
+            builder.append(" ");
         }
-        return str;
+        return builder.toString();
     }
 
 
     public String piramid(int h){
         int max=(h-1)*2+1;
-        String result="";
         StringBuilder builder = new StringBuilder();
         for(int i=1;(h>0)&(i<=h);i++){
             builder.append(this.PrintLine((i-1)*2+1,max));
             builder.append(System.getProperty("line.separator"));
         }
-        result=builder.toString();
-        return result;
+        return builder.toString();
     }
 }

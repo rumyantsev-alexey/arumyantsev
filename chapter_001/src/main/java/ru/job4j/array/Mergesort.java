abaa91628fdf66daa27bed5aa5b838ea88fda905
Alexey Rumyantsev <telesyn73@mail.ru>
2017-12-02 03:19:33 +0200

    Сортировка слиянием (исправил имена)

--- chapter_001/src/main/java/ru/job4j/array/Mergesort.java
+++ chapter_001/src/main/java/ru/job4j/array/Mergesort.java
@@ -7,16 +7,16 @@ public class Mergesort {
 
     /**
      * Слияние двух отсортированных массивов в третий отсортированный
-     * @param arr1 первый массив
-     * @param arr2 второй массив
+     * @param left первый массив
+     * @param right второй массив
      * @return результирующий массив
      *
      */
 
-    public int[] msort(int[] arr1, int[] arr2) {
+    public int[] msort(int[] left, int[] right) {
 
         // определяем результирующий массив суммарной длины 2х массивов
-        int[] resarr = new int[arr1.length + arr2.length];
+        int[] resarr = new int[left.length + right.length];
 
         // инициализируем счетчики 3х массивов
         int i= 0;
@@ -24,16 +24,16 @@ public class Mergesort {
         int k=0;
 
         //сравниваем элементы массива попарно, пока не кончиться один из массивов
-        while (i < arr1.length && j < arr2.length)
-            resarr[k++]=arr1[i]<=arr2[j]?arr1[i++]:arr2[j++];
+        while (i < left.length && j < right.length)
+            resarr[k++]=left[i]<=right[j]?left[i++]:right[j++];
 
         // смотрим какой из массивов первым закончился и копируем остатки второго массива в результирующий
-        if (i == arr1.length )
-            for (int m=j;m<=arr2.length-1;m++)
-                resarr[k++]=arr2[m];
+        if (i == left.length )
+            for (int m=j;m<=right.length-1;m++)
+                resarr[k++]=right[m];
         else
-            for (int m=i;m<=arr1.length-1;m++)
-                resarr[k++]=arr1[m];
+            for (int m=i;m<=left.length-1;m++)
+                resarr[k++]=left[m];
 
         return resarr;
     }

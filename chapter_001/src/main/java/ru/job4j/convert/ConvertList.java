534e92972eb6061df4085b77c9ea4d4d693836ab
Alexey Rumyantsev <telesyn73@mail.ru>
2018-01-16 17:46:16 +0200

    Конвертация двумерного массива в ArrayList и наоборот

---
+++ chapter_001/src/main/java/ru/job4j/convert/ConvertList.java
@@ -0,0 +1,54 @@
+package ru.job4j.convert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс для листа в 2д массив и обратно.
+ * @author Alex Rumyantcev
+ * @version $Id$
+ */
+public class ConvertList {
+
+    /**
+     * Метод для перевода 2д массива в список.
+     * @param array 2д массив
+     * @return полученный список
+     */
+    public static List<Integer> toList (final int[][] array) {
+        // создаем список
+        List<Integer> result = new ArrayList<Integer>();
+        // перебераем элементы массива
+        for (int[] i: array) {
+            for (int j: i) {
+                result.add(j);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод для перевода списка в 2д массив.
+     * @param list список
+     * @param rows количество строк в массиве
+     * @return поученный массив
+     */
+    public static int[][] toArray (final List<Integer> list, final int rows) {
+        // расчитываем размеры массива
+        // количество строк
+        final int sizer = rows;
+        // количество столбцов
+        final int sizec = (list.size() % rows) > 0 ? list.size() / rows + 1 : list.size() / rows;
+        // создаем массив
+        int[][] result = new int[sizer][sizec];
+        // счетчик элементов в списке
+        int counter = 0;
+        // перебераем элементы в списке
+        for (Integer i: list) {
+            result[counter / sizec][counter % sizec] = i;
+            counter++;
+        }
+        return result;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/convert/ConvertListTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.convert;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConvertListTest {
+
+    /**
+     * Тест перевода массива в список.
+     * Тест сравнивает размер полученного списка с количеством элементов массива
+     */
+    @Test
+    public void getArrayToList() {
+        Random randomGenerator = new Random();
+        // количество строк в массиве
+        final int sizer=3;
+        // количество столбцов в массиве
+        final int sizec=3;
+        // инициализируем массив
+        int[][] source=new int[sizer][sizec];
+        // заполняем массив случайными целыми
+        for (int i=0; i< sizer; i++) {
+            for (int j=0; j<sizec; j++) {
+                source[i][j]=randomGenerator.nextInt();
+            }
+        }
+        List<Integer> result= new ArrayList<>();
+        result=ConvertList.toList(source);
+        // сравниваем размер списка и массива
+        assertThat(result.size(), is(sizer*sizec));
+    }
+
+    /**
+     * Тест перевода списка в массив.
+     * Тест сравнивает значение одного и тогоже элемента в списке и массиве
+     */
+    @Test
+    public void getListToArray() {
+        Random randomGenerator = new Random();
+        // размер списка
+        final int size=16;
+        List<Integer> source = new ArrayList<>();
+        // заполняем список случайными значениями
+        for (int i=0; i<size;i++ ) {
+            source.add(randomGenerator.nextInt());
+        }
+        // определяем массив
+        int[][] result;
+        result=ConvertList.toArray(source, 3);
+        // сравниваем значения элемента
+        assertThat(result[2][1], is(source.get(13)));
+    }
+}

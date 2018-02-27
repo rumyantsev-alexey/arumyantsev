c7ab7fb54471cb1c1629ec322c67d07a5dbd7071
Alexey Rumyantsev <telesyn73@mail.ru>
2018-02-27 23:22:09 +0200

    Реализовать коллекцию Set на массиве [#996] v2

--- chapter_001/src/test/java/ru/job4j/set/SimpleSetTest.java
+++ chapter_001/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
@@ -7,23 +8,49 @@ import java.util.Iterator;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * Тесты для множества на основе массива с типом Integer
+ */
 public class SimpleSetTest {
+
+    SimpleSet<Integer> simple = new SimpleSet<>();
+
+    @Before
+    public void initValues() {
+        simple.add(1);
+        simple.add(2);
+        simple.add(3);
+        simple.add(4);
+    }
 
     @Test
     /**
-     * Тесты для контейнера с типом String
+     * Тест проверяет размер хранилища, затем добавляет объект и проверяет
+     * увеличился ли размер массива. Затем пробует добавить в хранилище уже
+     * существующий элемент и псле этого проверяет изменение размера хранилища
      */
-    public void getAllTestForString() {
-        SimpleSet<String> simple = new SimpleSet<>();
-        simple.add("first");
-        simple.add("second");
-        simple.add("third");
-        simple.add("four");
+    public void testAdd() {
         assertThat(simple.size(), is(4));
-        simple.add("first");
-        assertThat(simple.size(), is(4));
-        Iterator<String> simpleiter = simple.iterator();
-        assertThat(simpleiter.next(),is("first"));
+        simple.add(5);
+        assertThat(simple.size(), is(5));
+        simple.add(1);
+        assertThat(simple.size(), is(5));
+    }
+
+    @Test
+    /**
+     * Тест проверяет работоспособность итератора.
+     * Сначало проверяем наличие объекта, потом получаем первый элемент. Проверяем
+     * наличие следующего. Переходим в конец множества и проверяем что больше элементов нет.
+     */
+    public void testIterator() {
+        Iterator<Integer> simpleiter = simple.iterator();
         assertThat(simpleiter.hasNext(), is(true));
+        assertThat(simpleiter.next(),is(1));
+        simpleiter.next();
+        assertThat(simpleiter.hasNext(), is(true));
+        simpleiter.next();
+        simpleiter.next();
+        assertThat(simpleiter.hasNext(), is(false));
     }
 }

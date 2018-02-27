dd1414d50778f7cb3aaa218560c8a015b1850590
Alexey Rumyantsev <telesyn73@mail.ru>
2018-02-27 23:11:45 +0200

    Set реализованный на связном списке. [#997] v2

--- chapter_001/src/test/java/ru/job4j/set/SimpleListSetTest.java
+++ chapter_001/src/test/java/ru/job4j/set/SimpleListSetTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
@@ -8,23 +9,49 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+/**
+ * Тесты для множества на основе связанного списка с типом String
+ */
 public class SimpleListSetTest {
 
-    @Test
-    /**
-     * Тесты для контейнера с типом String
-     */
-    public void getAllTestForString() {
-        SimpleListSet<String> simple = new SimpleListSet<>();
+    SimpleListSet<String> simple = new SimpleListSet<>();
+
+    @Before
+    public void initValues() {
         simple.add("first");
         simple.add("second");
         simple.add("third");
         simple.add("four");
+    }
+
+    @Test
+    /**
+     * Тест проверяет размер хранилища, затем добавляет объект и проверяет
+     * увеличился ли размер массива. Затем пробует добавить в хранилище уже
+     * существующий элемент и псле этого проверяет изменение размера хранилища
+     */
+    public void testAdd() {
         assertThat(simple.size(), is(4));
+        simple.add("five");
+        assertThat(simple.size(), is(5));
         simple.add("first");
-        assertThat(simple.size(), is(4));
+        assertThat(simple.size(), is(5));
+    }
+
+    @Test
+    /**
+     * Тест проверяет работоспособность итератора.
+     * Сначало проверяем наличие объекта, потом получаем первый элемент. Проверяем
+     * наличие следующего. Переходим в конец множества и проверяем что больше элементов нет.
+     */
+    public void testIterator() {
         Iterator<String> simpleiter = simple.iterator();
-        assertThat(simpleiter.next(),is("first"));
         assertThat(simpleiter.hasNext(), is(true));
+        assertThat(simpleiter.next(),is("first"));
+        simpleiter.next();
+        assertThat(simpleiter.hasNext(), is(true));
+        simpleiter.next();
+        simpleiter.next();
+        assertThat(simpleiter.hasNext(), is(false));
     }
 }

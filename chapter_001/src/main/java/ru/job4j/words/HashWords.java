0458a6491fe96b1c199123d1ae30244570df6312
Alexey Rumyantsev <telesyn73@mail.ru>
2018-04-11 12:44:34 +0300

    Сравнение двух слов  v 2.1

--- chapter_001/src/main/java/ru/job4j/words/HashWords.java
+++ chapter_001/src/main/java/ru/job4j/words/HashWords.java
@@ -3,29 +3,22 @@ package ru.job4j.words;
 import java.util.HashMap;
 
 /**
- * Класс сравнивает два слова между собой, раскидав первое слово в hashmap, где ключ это символ, а значение -
+ * Класс сравнивает два слова между собой на наличие одинаковых букв, раскидав первое слово в hashmap, где ключ это символ, а значение -
  * количество этих символов в слове
  */
 public class HashWords {
 
-    public static boolean compareTwoWords(String word, String otherword) {
-        char [] w = word.toCharArray();
-        char [] ow = otherword.toCharArray();
+    /**
+     * Метод сравнивает два слова
+     * @param word первое словаа
+     * @param otherword второе слово
+     * @return результат сравнения
+     */
+    public static boolean compareTwoWords(final String word, final String otherword) {
         HashMap<Character, Integer> array = new HashMap<>();
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-            array.put((Character) w[i], array.containsKey((Character) w[i]) ? array.get((Character) w[i])+1 : 1);
-        }
-        for (int i = 0; i < ow.length; i++) {
-            array.put((Character) ow[i], array.get((Character) ow[i])-1);
-        }
-        for (Integer v: array.values()) {
-            if (v != 0) {
-                result = v;
-                break;
-            }
-        }
-    return result == 0;
+        word.chars().mapToObj(c -> (char) c).forEach(s -> array.put(s, array.containsKey(s) ? array.get(s) + 1 : 1));
+        otherword.chars().mapToObj(c -> (char) c).forEach(s -> array.put(s, array.get(s) - 1));
+        return array.values().stream().filter(s -> s != 0).findFirst().orElse(0) == 0;
     }
 
     }

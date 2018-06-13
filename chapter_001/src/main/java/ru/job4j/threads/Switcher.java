7b3b5d5de1290a2cbe87e9ebe679801af4ecbd15
Alexey Rumyantsev <telesyn73@mail.ru>
2018-06-13 17:21:52 +0300

    Свитчер [#50581]

---
+++ chapter_001/src/main/java/ru/job4j/threads/Switcher.java
@@ -0,0 +1,110 @@
+package ru.job4j.threads;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Класс реализует тестовую задачу Switcher
+ */
+public class Switcher {
+    //количество символов передаваемых потоком за один цикл
+    private static final int COUNT = 10;
+    private final StringBuffer string = new StringBuffer();
+    private volatile int cntOne  = COUNT;
+    private volatile int cntTwo  = COUNT;
+    private final ReentrantLock lckOne = new ReentrantLock();
+    private final ReentrantLock lckTwo = new ReentrantLock();
+
+    /**
+     * Метод возвращет текущую строку
+     * @return строка
+     */
+    public StringBuffer getString() {
+        return string;
+    }
+
+    /**
+     * Метод добавляет число в строку
+     * @param number число
+     */
+    public void add(int number) {
+        string.append(number);
+    }
+
+    /**
+     * Поток, добавляющий 1 к строке 10 раз
+     */
+    protected Thread thread1 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            boolean result;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    result = lckOne.tryLock(500, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    continue;
+                }
+                if (result) {
+                    while (cntOne > 0 && !Thread.currentThread().isInterrupted()) {
+                        add(1);
+                        cntOne--;
+                    }
+                    lckOne.unlock();
+                }
+                try {
+                    result = lckTwo.tryLock(500, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    continue;
+                }
+                if (result) {
+                    cntTwo = COUNT;
+                    lckTwo.unlock();
+                }
+                System.out.println("Thread1 - current string - "+string);
+                while (cntTwo > 0 && !Thread.currentThread().isInterrupted()) {
+
+                }
+            }
+            System.out.println("Thread1 end");
+        }
+    });
+
+    /**
+     * Поток, добавляющий 2 к строке 10 раз
+     */
+    protected  Thread thread2 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            boolean result;
+            while (!Thread.currentThread().isInterrupted()) {
+                 while(cntOne > 0 && !Thread.currentThread().isInterrupted()) {
+
+                 }
+                try {
+                    result = lckTwo.tryLock(500, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    continue;
+                }
+                if (result) {
+                    while (cntTwo > 0 && !Thread.currentThread().isInterrupted()) {
+                        add(2);
+                        cntTwo--;
+                    }
+                    lckTwo.unlock();
+                }
+                try {
+                    result = lckOne.tryLock(500, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    continue;
+                }
+                if (result) {
+                    cntOne = COUNT;
+                    lckOne.unlock();
+                }
+                System.out.println("Thread2 - current string - "+ string);
+            }
+            System.out.println("Thread2 end");
+        }
+    });
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/threads/SwitcherTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.threads;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Тест для задачи Switcher
+ */
+public class SwitcherTest {
+
+    @Test
+    public void testSwitch() {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        Switcher sw = new Switcher();
+
+        pool.execute(sw.thread1);
+        pool.execute(sw.thread2);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        pool.shutdownNow();
+        assertThat(pool.isShutdown(), is(true));
+        try {
+            pool.awaitTermination(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Result:");
+        System.out.println(sw.getString());
+    }
+}

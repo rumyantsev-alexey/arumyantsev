dac41fc46c893abfe940d629f309e73b11e215cd
Alexey Rumyantsev <telesyn73@mail.ru>
2018-07-15 21:09:57 +0300

    struct cosmetik v0.1

--- chapter_001/src/main/java/ru/job4j/bombermen/Direction.java
+++
@@ -1,3 +0,0 @@
-package ru.job4j.bombermen;
-
-public enum Direction {STOP, UP, DOWN, LEFT, RIGHT}
--- chapter_001/src/main/java/ru/job4j/bombermen/Type.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.bombermen;
-
-/**
- *Типы возможных фигур в игре
- */
-public enum Type {HERO, MONSTER, WALL}
-
-

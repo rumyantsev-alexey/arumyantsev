bfb13662f191d0b61493301f713ae3ddbff2ae44
alex <telesyn73@mail.ru>
2019-06-15 02:43:10 +0300

    Список адресов [#110062]

---
+++ chapter_003/src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+public class Profile {
+
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}

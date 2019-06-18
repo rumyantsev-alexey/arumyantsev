3478600f343f5deb5245d5d2e64ec5e5d433f7a6
alex <telesyn73@mail.ru>
2019-06-18 21:43:19 +0300

    Stream API улучшения [#58167]

--- chapter_003/src/main/java/ru/job4j/stream/School.java
+++ chapter_003/src/main/java/ru/job4j/stream/School.java
@@ -3,8 +3,11 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class School {
+    StudentScoreComparator ssc = new StudentScoreComparator();
 
     List<Student> collect(List<Student> students, Predicate<Student> predict) {
         List<Student> result = new ArrayList<>();
@@ -14,5 +17,10 @@ public class School {
             }
         }
         return result;
+    }
+
+    List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream().flatMap(Stream::ofNullable).sorted(ssc.reversed()).takeWhile(s -> s.getScore() > bound).collect(Collectors.toList());
+
     }
 }
